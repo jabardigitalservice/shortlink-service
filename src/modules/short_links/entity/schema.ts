93519822f956d0da161556ec51ff30a2828b4303
ayocodingit <ayocodingit@gmail.com>
2024-01-15 11:41:13 +0700

    update validasi short link

--- src/modules/short_links/entity/schema.ts
+++ src/modules/short_links/entity/schema.ts
@@ -1,8 +1,7 @@
 import Joi from 'joi'
-import { RegexSubdomain } from '../../../helpers/regex'
 
 export const RequestSchema = Joi.object({
-    short_link: Joi.string().regex(RegexSubdomain).optional(),
+    short_link: Joi.string().alphanum().optional(),
     url: Joi.string().uri().required(),
     expired: Joi.date().min(new Date()).optional(),
 })
