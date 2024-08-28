1ac605500799df2c43e0e620be21205e174c6d7e
ayocodingit <ayocodingit@gmail.com>
2024-08-28 16:54:51 +0700

    fix not found

--- src/transport/http/http.ts
+++ src/transport/http/http.ts
@@ -25,7 +25,7 @@ class Http {
         this.app.use(bodyParser.json())
         this.app.use(helmet())
         this.app.use(compression())
-        this.app.use('/page/404', express.static('./src/page/404.html'))
+        this.app.use('/page/404', express.static('build/src/page/404.html'))
     }
 
     private pageNotFound = () => {
